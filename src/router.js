9cbaac180f150312592b7742e2691e97cf02b39c
CamilaRLS <70521820+CamilaRLS@users.noreply.github.com>
2020-10-01 18:37:41 -0300

    commit

--- src/router.js
+++ src/router.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
-import Portfolio from './pages/Portfolio';
+import Portifolio from './pages/Portifolio';
 import Quemsomos from './pages/Quemsomos';
 
 export default function Routes(){
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Home} />
+                <Route path="/home" exact component={Home} />
                 <Route path="/portifolio"  component={Portifolio} />
                 <Route path="/quemsomos"  component={Quemsomos} />
             </Switch>
